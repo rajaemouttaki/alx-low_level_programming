cc19078c474af4fc3b8d16851f478405ebb1a483
rajaemouttaki <rajaemouttaki630@gmail.com>
2023-09-07 13:09:06 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
 /**
-* main - Entry point
- * puts message
- * Return: 0
- */
-
+*main - Main function of my project
+*
+*
+*Return: return 0 everytime
+*/
 int main(void)
-
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	char displayed[52];
 
-}	
+	strcpy(displayed, "\"Programming is like building a multilingual puzzle");
+	puts(displayed);
+	return (0);
+}
