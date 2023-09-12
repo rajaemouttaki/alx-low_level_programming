78f1fdf9520004e01e90e5be0c24c3ade7599ba3
rajaemouttaki <rajaemouttaki630@gmail.com>
2023-09-12 15:17:52 -0700

    #include main.h
    /**
    *main - function prints text as output
    *
    *Return:return 0
    */
    int main(void)
    {
    char school[10] = _putchar;
    
    int i;
    
    for (i = 0; i < 8; i++)
    {
    _putchar(school[i]);
    }
    _putchar(10);

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,19 @@
 #include "main.h"
 
-
 /**
- * main - Entry point
- *
- * Return: nothing, return void
- */
-
+*main - function prints text as output
+*
+*Return:return 0
+*/
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+	char school[10] = "_putchar";
 
-return (0);
-}
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(school[i]);
+	}
+	_putchar(10);
+	return (0);
