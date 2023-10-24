4333812cee41d22da67ddeb603c17781a0417524
rajaemouttaki <132359546+rajaemouttaki@users.noreply.github.com>
2023-10-24 00:19:20 +0000

    chmint_listint.c

--- 0-print_listint.c
+++ 0-print_listint.c
@@ -1,21 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a linked list
- * @h: head of the list
- *
- * Return: the number of nodes
+ * main - check the code
+ * 
+ * Return: Always 0.
  */
-size_t print_listint(const listint_t *h)
+int main(void)
 {
-const listint_t *cursor = h;
-size_t count = 0;
+    listint_t *head;
+    listint_t *new;
+    listint_t hello = {8, NULL};
+    size_t n;
 
-while (cursor != NULL)
-{
-printf("%d\n", cursor->n);
-count += 1;
-cursor = cursor->next;
-}
-return (count);
+    head = &hello;
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+    {
+        printf("Error\n");
+        return (1);
+    }
+    new->n = 9;
+    new->next = head;
+    head = new;
+    n = print_listint(head);
+    printf("-> %lu elements\n", n);
+    free(new);
+    return (0);
 }
